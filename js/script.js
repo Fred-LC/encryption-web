6a2722665b0461e6a50ff062e53ec6d8585348bd
Frédéric Le Crom <frederic.lecrom@gmail.com>
2025-01-11 23:06:03 +0100

    Fonctionnement du formulaire de chiffrement / déchiffrement d'un texte.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,39 @@
 jQuery(function() {
+    $('#form').on('submit', function (event) {
+        event.preventDefault();
 
+        processForm('decrypt');
+    });
+
+    $('#form').on('click', '#decrypt', function (event) {
+        event.preventDefault();
+
+        processForm('decrypt');
+    });
+
+    $('#form').on('click', '#encrypt', function (event) {
+        processForm('encrypt');
+    });
+
+    function processForm(operation = 'decrypt') {
+        var content = $('#form #content').val();
+        var key = $('#form #key').val();
+        var result = '';
+
+        if ('decrypt' === operation) {
+            result = decrypt(content, key);
+        } else {
+            result = encrypt(content, key);
+        }
+
+        $('#result').html(result);
+    }
+
+    function decrypt(content, key) {
+        return Aes.Ctr.decrypt(content, key, 256);
+    }
+
+    function encrypt(content, key) {
+        return Aes.Ctr.encrypt(content, key, 256);
+    }
 });
